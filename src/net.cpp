66eecfcdb0d7f1cf617b2d6bf04afec33270950e
tamag901 <quahmingsheng@hotmail.com>
2017-06-19 20:56:05 +0800

    Set server to localhost

--- src/net.cpp
+++ src/net.cpp
@@ -1192,11 +1192,7 @@ void MapPort(bool)
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strMainNetDNSSeed[][2] = {
-    {"leapcointools.com", "dnsseed.leapcointools.com"},
-    {"leapcoinpool.org", "dnsseed.leapcoinpool.org"},
-    {"xurious.com", "dnsseed.ltc.xurious.com"},
-    {"koin-project.com", "dnsseed.koin-project.com"},
-    {"weminemnc.com", "dnsseed.weminemnc.com"},
+    {"localhost", "127.0.0.1"},//CHANGE THIS
     {NULL, NULL}
 };
 
